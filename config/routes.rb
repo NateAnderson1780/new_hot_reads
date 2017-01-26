e30711963bc99b2439864fd42b3090e8e3cb0748
Nate Anderson <curtislowe10@gmail.com>
2017-01-25 19:57:29 -0700

    alter routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "hot_reads#index"
   
-  resources :hot_reads, only: [:create, :index]
+  resources :hot_reads, only: [:index]
   
   namespace :api do
     namespace :v1 do
-      resources :hot_reads, only: [:index]
+      resources :hot_reads, only: [:index, :create]
     end
   end
 end
